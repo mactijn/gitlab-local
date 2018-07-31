c25ba95757a42fa6bc84f43aa066e45b2d21f543
Martijn Bakker <martijn@mactijn.eu>
2018-07-31 21:06:47 +0200

    run gitlab-ce privileged so we do not have to fall back to inplace_chroot for pages

--- gitlab/gitlab.rb
+++ gitlab/gitlab.rb
@@ -7,7 +7,6 @@ registry['registry_http_addr'] = "localhost:4999"
 gitlab_rails['registry_api_url'] = "http://localhost:4999"
 
 pages_external_url "http://pages.localhost"
-gitlab_pages['inplace_chroot'] = true
 
 gitlab_rails['env'] = { 'prometheus_multiproc_dir' => '/prometheus' }
 gitlab_rails['gitlab_email_enabled'] = false
